030d0cc724159d79f59d0eda0b7fb90e4e736bb3
Iain Maitland <iain.maitland@wayflyer.com>
2024-09-04 15:38:48 +0100

    do nothing in VS Code mode

--- .config/nvim/lua/iain/init.lua
+++ .config/nvim/lua/iain/init.lua
@@ -1,4 +1,5 @@
-require("iain.remap") -- has to come before lazy to remap leader correctly
-require("iain.lazy")
-require("iain.options")
-
+if not vim.g.vscode then
+  require("iain.remap") -- has to come before lazy to remap leader correctly
+  require("iain.lazy")
+  require("iain.options")
+end
