58142c8ecdbfd1a3826d273f6eb9e123b9810388
Iain Maitland <iain.maitland@wayflyer.com>
2024-09-10 19:01:02 +0100

    smart paths in telescope

--- .config/nvim/lua/iain/plugins/telescope.lua
+++ .config/nvim/lua/iain/plugins/telescope.lua
@@ -1,21 +1,28 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    -- tag = '0.1.8',
-	branch = '0.1.x',
-      dependencies = {
-	      'nvim-lua/plenary.nvim',
-	      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      },
-      config = function()
-        local telescope = require('telescope')
-	local builtin = require('telescope.builtin')
+  'nvim-telescope/telescope.nvim',
+  -- tag = '0.1.8',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
 
-	telescope.load_extension('fzf')
+    telescope.setup({
+      defaults = {
+        path_display = {"smart"},
+      }
+    })
 
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- searches all files
-	vim.keymap.set('n', '<leader>fs', builtin.live_grep, {}) -- searches content of all files
-	vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- ignores files in .gitignore
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      end
-    }
+    telescope.load_extension('fzf')
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- searches all files in cwd
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {}) -- searches recent files
+    vim.keymap.set('n', '<leader>fs', builtin.live_grep, {}) -- searches content of all files in cwd
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- like find_files except ignores files in .gitignore
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  end
+}
