e837d3d2b008395cefafd2589e3295c9167ee886
Iain Maitland <iain.maitland@wayflyer.com>
2025-09-13 21:11:51 +0100

    wip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -101,7 +101,7 @@ vim.opt.scrolloff = 10
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "iain.plugins" },
+    { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
