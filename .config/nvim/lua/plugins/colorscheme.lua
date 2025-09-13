643c11d8de29008d2e020f265df6266ec1ed4723
Iain Maitland <iain.maitland@wayflyer.com>
2025-09-13 22:31:38 +0100

    wip

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 900,
+	"projekt0n/github-nvim-theme",
+	name = "github-theme",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 900, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd([[colorscheme tokyonight]])
+		require("github-theme").setup({
+			-- ...
+		})
+
+		vim.cmd("colorscheme github_dark_dimmed")
 	end,
 }
