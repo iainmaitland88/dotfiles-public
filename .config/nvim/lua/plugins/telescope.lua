6d4fe6553cc918e4d8726c193253693ecb21bab7
Iain Maitland <iain.maitland@wayflyer.com>
2025-09-13 21:45:40 +0100

    wip

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
 return {
   'nvim-telescope/telescope.nvim',
-  enabled = false,
   -- tag = '0.1.8',
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    'BurntSushi/ripgrep',
   },
   config = function()
     local telescope = require('telescope')
@@ -19,11 +19,11 @@ return {
 
     telescope.load_extension('fzf')
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- searches all files in cwd
-    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {}) -- searches recent files
-    vim.keymap.set('n', '<leader>fs', builtin.live_grep, {}) -- searches content of all files in cwd
-    vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- like find_files except ignores files in .gitignore
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- searches files in cwd
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, {})  -- searches files in Git repo (ignores files in .gitignore)
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})   -- searches recent files
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})    -- searches files in buffer
+    vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})  -- searches content of all files in cwd
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})  -- searches in help tags
   end
 }
