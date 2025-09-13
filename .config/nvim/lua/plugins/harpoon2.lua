47e17300875e6fb8c1ee7f83054e949944f2a2c2
Iain Maitland <iain.maitland@wayflyer.com>
2025-09-13 21:55:30 +0100

    wip

--- .config/nvim/lua/plugins/harpoon2.lua
+++ .config/nvim/lua/plugins/harpoon2.lua
@@ -1,18 +1,35 @@
 return {
   "ThePrimeagen/harpoon",
-  enabled = false,
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local harpoon = require("harpoon")
-
-    -- REQUIRED
     harpoon:setup()
-    -- REQUIRED
+
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+            table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers").new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+                results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+        }):find()
+    end
+
+    vim.keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
 
     -- might not stick with these remappings, just until I find something better
     vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    -- view harpooned files with harpoon's explorer
+    -- vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
