824e282218c041de3f4ca72aa696e7f10652b8bb
Iain Maitland <iain.maitland@wayflyer.com>
2025-09-13 23:17:56 +0100

    wip

--- .wezterm.lua
+++ .wezterm.lua
@@ -24,5 +24,24 @@ config.window_padding = {
   bottom = 0,
 }
 
+local act = wezterm.action
+
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
+  },
+}
+
 -- Finally, return the configuration to wezterm:
 return config
