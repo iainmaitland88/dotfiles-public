de942106965a29fad823f4b7580c8feb3cf0ebe8
Iain Maitland <iain.maitland@wayflyer.com>
2025-10-10 21:42:35 +0100

    Fix wezterm splits key bindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -61,12 +61,12 @@ config.keys = {
 	{
 		key = '"',
 		mods = "LEADER",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 	{
 		key = "%",
 		mods = "LEADER",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
 		key = "z",
