e9445d87b37c908282e259e9b7d5471cb7a5093f
Iain Maitland <iain.maitland@wayflyer.com>
2025-11-29 23:56:54 +0000

    Let's try catppuccin again

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	lazy = false,
 	priority = 900,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
